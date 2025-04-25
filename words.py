f6e6fadeda5f9b941478da52056afc74e49cf87e
Noushad Akhter <163678090+noushadakhter@users.noreply.github.com>
2025-04-25 20:20:47 +0500

    Delete words.py

--- words.py
+++
@@ -1,8 +0,0 @@
-# words.py
-
-words = [
-    "abandon", "ability", "able", "about", "above", "absent", "absorb", "abstract", "absurd", "access",
-    "accident", "account", "accuse", "achieve", "acoustic", "acquire", "across", "act", "action", "actor",
-    "actress", "actual", "adapt", "add", "addict", "address", "adjust", "admit", "adult", "advance",
-    # ... You can add more words here to make the game more fun ...
-]
